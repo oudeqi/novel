41b4104eface6d19a17aa1528604400db28811cf
oudeqi <496997374@qq.com>
2017-05-18 18:00:10 +0800

    网站设置界面

---
+++ src/component/site-settings/site-settings.controller.js
@@ -0,0 +1,14 @@
+;(function(){
+    'use strict';
+    angular.module('app').controller('site-settings',['$scope','$http',
+        function($scope,$http){
+
+            $scope.weixingongzhong = true;
+
+
+
+
+
+        }
+    ]);
+})();
