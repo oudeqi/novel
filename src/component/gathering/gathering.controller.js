d6fcf046460777c8ed9c076d7af7d8472677fea5
439407962@qq.com <439407962@qq.com>
2017-05-19 19:56:03 +0800

    gathering add

--- src/component/gathering/gathering.controller.js
+++ src/component/gathering/gathering.controller.js
@@ -1,15 +1,23 @@
 ;(function(){
     'use strict';
-    angular.module('app').controller('agentctl',['$scope','$http','$stateProvider','$urlRouterProvider'
-        function($scope,$http,$stateProvider,$urlRouterProvider){
-        	$stateProvider.state({
+    
+    
+    angular.module('app').controller('gathering',['$scope','$http',
+        function($scope,$http){
+        	
+        	var k=$scope;
+			
+
+
+        }
+    ])
+    .config(['$stateProvider','$urlRouterProvider',
+        function($stateProvider,$urlRouterProvider){
+            $stateProvider.state({
                 name: 'warpper.views.section.gathering',
                 url: '^/gathering',
                 templateUrl: './component/gathering/gathering.html'
            });
-        	var k=$scope;
-			
-
 
         }
     ]);
