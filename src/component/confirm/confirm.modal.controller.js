9e66946cfc9565d9e742208dcfcab74c23867c5f
oudeqi <496997374@qq.com>
2017-05-22 16:35:13 +0800

    修复章节列表第二页前五条也可以生成推广文案的bug

--- src/component/confirm/confirm.modal.controller.js
+++ src/component/confirm/confirm.modal.controller.js
@@ -4,9 +4,6 @@
     angular.module('app').controller('confirmCtrl',['$scope','$uibModalInstance','confirm',
         function($scope,$uibModalInstance,confirm){
 
-            console.log(confirm);
-
-
             $scope.title = confirm.title;
             $scope.msg = confirm.msg;
 
