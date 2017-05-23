495f06067590014e9c01c774166e748115921e9b
oudeqi <496997374@qq.com>
2017-05-23 18:35:52 +0800

    **

--- src/component/app/app.constant.js
+++ src/component/app/app.constant.js
@@ -1,12 +1,7 @@
 ;(function(){
 
     'use strict';
-<<<<<<< HEAD
     // angular.module('app').constant('APP_HOST','http://192.168.10.28:8080');
     angular.module('app').constant('APP_HOST','http://book.zbty8.com/');
-
-=======
-//  angular.module('app').constant('APP_HOST','http://192.168.10.28:8080');
-       angular.module('app').constant('APP_HOST','http://book.zbty8.com');
->>>>>>> 9070c73051f1499f9229d6826d621fb6f06edf79
+    
 })();
