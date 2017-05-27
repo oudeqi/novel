14ae32f8ba50fe05afa6313f8d03ef66d6e94991
439407962@qq.com <439407962@qq.com>
2017-05-27 09:53:09 +0800

    add 时间中文

--- src/component/order-list/order-list.js
+++ src/component/order-list/order-list.js
@@ -14,15 +14,37 @@
 
         }
     ]);
-    angular.module('app').controller('order-list',['$scope','$http','$state',
-        function($scope,$http,$state){
-
+    angular.module('app').controller('order-list',['$scope','$http','$state','$provide',
+        function($scope,$http,$state,$provide){
+			
+			var PLURAL_CATEGORY = {ZERO: "zero", ONE: "one", TWO: "two", FEW: "few", MANY: "many", OTHER: "other"};
+			$provide.value("$locale", {"DATETIME_FORMATS":{"MONTH":["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],"SHORTMONTH":["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"],"DAY":["星期日","星期一","星期二","星期三","星期四","星期五","星期六"],"SHORTDAY":["周日","周一","周二","周三","周四","周五","周六"],"AMPMS":["上午","下午"],"medium":"yyyy-M-d ah:mm:ss","short":"yy-M-d ah:mm","fullDate":"y年M月d日EEEE","longDate":"y年M月d日","mediumDate":"yyyy-M-d","shortDate":"yy-M-d","mediumTime":"ah:mm:ss","shortTime":"ah:mm"},"NUMBER_FORMATS":{"DECIMAL_SEP":".","GROUP_SEP":",","PATTERNS":[{"minInt":1,"minFrac":0,"macFrac":0,"posPre":"","posSuf":"","negPre":"-","negSuf":"","gSize":3,"lgSize":3,"maxFrac":3},{"minInt":1,"minFrac":2,"macFrac":0,"posPre":"\u00A4","posSuf":"","negPre":"\u00A4-","negSuf":"","gSize":3,"lgSize":3,"maxFrac":2}],"CURRENCY_SYM":"¥"},"pluralCat":function (n) {  return PLURAL_CATEGORY.OTHER;},"id":"zh-cn"});
+            
+            
             $scope.maxSize = 5;
             $scope.totalItems = 0;
             $scope.currentPage = 1;
             $scope.pageSize = 20;
             $scope.pageCount = 0;//总页数
             $scope.list = [];
+            
+            /*时间筛选相关*/
+             $scope.dateOptions = {
+//			    dateDisabled: visible,
+				language:'cn',
+			    formatYear: 'yy',
+			    maxDate: new Date(),
+			    minDate: new Date(2000,5,22),
+			    startingDay: 1
+			  };
+            $scope.popup2 = {
+			    opened: false
+			  };
+           	$scope.dt = new Date();
+           	$scope.open2 = function() {
+			    $scope.popup2.opened = true;
+			  };
+            
 
             $scope.status = 0;//1待支付，2已支付
 
